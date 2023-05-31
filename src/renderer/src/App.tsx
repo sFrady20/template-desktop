c91a29ca8e6045701543224b2ad7d351cf3c4d13
Steven Frady <sfrady20@gmail.com>
2023-05-31 18:06:05 -0400

    routing and layouts

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,17 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import DefaultLayout from "./layouts/Default";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: DefaultLayout,
+    children: [{ path: "/", Component: HomePage }],
+  },
+]);
+
 function App(): JSX.Element {
-  return <div>Desktop template</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
